0e098dd6863a28ee683ab7cd51e6f9404730c065
Mia Hawkins <ilearnarcheology@aol.com>
2018-02-19 17:29:37 -0600

    add routing for saved articles

--- nytimes/router/router.js
+++ nytimes/router/router.js
@@ -1,10 +1,45 @@
 var express = require('express');
 var router = express.Router();
 
-// Import models
 var Article = require('../models/Article');
 
+router.get("/", function(req, res) {
+    res.sendFile(process.cwd() + "/public/index.html");
+})
 
+//All the save Articles routing as follows..../////////////////
+//get all saved articles
+router.get("/api/saved", function(req, res) {
+    Article.find({}, function(err, info) {
+        if(err) {
+            console.log(err);
+        }else {
+            res.json(info);
+        }
+    });
+});
+
+//to save an article to db
+router.post("/api/saved", function(req, res) {
+    Article.save(function(err, info) {
+        if(err) {
+            console.log(err);
+        }else {
+            console.log(info);
+        }
+    });
+});
+
+//to delete a saved article from db
+router.post("/api/saved", function(req, res) {
+    Article.findByIdAndRemove(req.params.articleID,function(err, info) {
+        if(err) {
+            console.log(err);
+        }else {
+            console.log(info);
+        }
+    });
+});
 
 // Export routes.
 module.exports = router;
